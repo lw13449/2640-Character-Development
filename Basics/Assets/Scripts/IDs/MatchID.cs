865032427f51e75c06d81f61dd68b393686d3d67
Lillian Wright <10803934@my.uvu.edu>
2020-01-14 12:51:08 -0700

    Trigger Events and Name IDs

---
+++ Basics/Assets/Scripts/IDs/MatchID.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchID : MonoBehaviour
+{
+    public NameID nameIDOBJ;
+        
+    private void OnTriggerEnter(Collider other)
+    {
+        var otherNameID = other.GetComponent<MatchID>().nameIDOBJ;
+        if (nameIDOBJ == null) return;
+        var otherNameID = nameIDOBJ;
+        if (nameIDOBJ == otherNameID)
+        {
+            //do work
+        }
+
+    }
+    //check for multiple objects; check to see if it is more than one object. Like how many NameIDs it can have
+    //the problem with the name and tag is it can only have one thing. 
+    //How can I take an object running into another object and check it for multiple nameIDs
+}
