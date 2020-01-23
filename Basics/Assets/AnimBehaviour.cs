664ec689408b44949f69f6503461c7f6aaf1d8b5
Lillian Wright <10803934@my.uvu.edu>
2020-01-23 12:47:07 -0700

    Animation Controller and Shifting States
    AnimController Script
    ScriptableObject Scripts
    Animation Controller States

---
+++ Basics/Assets/AnimBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimBehaviour : StateMachineBehaviour
+{
+    public UnityEvent stateEnterEvent;
+    
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        stateEnterEvent.Invoke();
+    }
+}
