318455c9b3815cc1d21239f18cd95a8eb1e2a088
Lillian Wright <10803934@my.uvu.edu>
2020-01-29 18:30:52 -0700

    ID script and Animation interface
    Object ID script for Match ID set, and Animation script, as well as messing with the animator in Unity

---
+++ Basics/Assets/Scripts/Animation Scripts/AnimationScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour
+{
+    public Animator animatorObj;
+    public void Update()
+	{
+		
+	}
+}
