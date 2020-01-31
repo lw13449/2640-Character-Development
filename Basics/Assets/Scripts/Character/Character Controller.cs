c337a65e7ecaf6c0f66bebcced902c4dccdab782
Lillian Wright <10803934@my.uvu.edu>
2020-01-31 15:24:04 -0700

    Character Controller with rotate, Lists, and Applying force attempt 1
    Added Rotate for 3D space with Character controller. Thinking about utilizing the "do work" in the MatchID script with Searching Inventory. Too narrow?
    Started Inventory with Lists, unsure if this is the right path to go down for this. Attempting to create efficient list types for game.
    Apply Force script did not entirely work for the first attempt. It seems that only ridgedbodies make this work. More research needed to figure out how to take ridgedbody out of the solution so it doesn't battle for dominance with player.

--- Basics/Assets/Scripts/Character/Character Controller.cs
+++ Basics/Assets/Scripts/Character/Character Controller.cs
@@ -7,9 +7,11 @@ public class CharacterController : MonoBehaviour
     private CharacterController controller;
 
     public float moveSpeed = 10f, gravity = 9.81f, jumpSpeed = 30f;
+    public float rotateSpeed = 2;
     private int jumpCount;
     public int jumpCountMax = 2;
-    
+    public bool isGrounded;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -17,6 +19,24 @@ public class CharacterController : MonoBehaviour
     
     void Update()
     {
-        
+        Move(Input.GetAxis("Vertical"));
+        Rotate(Input.GetAxis("Horizontal"));
+    }
+    
+
+    public void Move(float myInput)
+    {
+        position.x = myInput;
+        position = transform.TransformDirection(position * moveSpeed * Time.deltaTime);
+    }
+    void Rotate(float myInput)
+    {
+        transform.Rotate(0, myInput*rotateSpeed, 0);
+    }
+
+    void SearchInventory()
+    {
+        //the do work section of creating the Inventory List for objects, floats, and others you gather during the game.
+        //Call the MatchID script to do work.
     }
 }
