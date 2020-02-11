4cfd056f62172d0b72d8624ced0103d761cde94f
Lillian Wright <10803934@my.uvu.edu>
2020-02-11 13:04:37 -0700

    match IDs and systems

---
+++ Basics/Assets/Scripts/FloatData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : NameID
+{
+    public float value;
+
+    public void SetValue(float amount)
+    {
+        value = amount;
+    }
+
+    public void UpdateValue(float amount)
+    {
+        value += amount;
+    }
+
+    public void ChangeValue(float amount)
+    {
+        value -= amount;
+    }
+}
