da130592a29215f8b58471a1461e1c24c6425b8d
Lillian Wright <10803934@my.uvu.edu>
2020-02-13 14:18:49 -0700

    setup

---
+++ Basics/Assets/Scripts/collections/Lists.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Lists : ScriptableObject
+{
+    public List<string> stringList;
+    public List<int> intList;
+    public List<float> floatList;
+    public List<GameObject> gameObjList;
+    public List<FloatData> floatDataList;
+}
