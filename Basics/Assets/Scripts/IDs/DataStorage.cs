71aa022b29f38ecfed9ca46466572465ce55aaa3
Lillian Wright <10803934@my.uvu.edu>
2020-02-26 23:20:26 -0700

    Color Data separation

---
+++ Basics/Assets/Scripts/IDs/DataStorage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu]
+public class DataStorage : ScriptableObject
+{
+    public ScriptableObject data;
+
+    public void SetData()
+    {
+        if (data == null) return;
+        PlayerPrefs.SetString(data.name, JsonUtility.ToJson(data));
+    }
+
+    public void GetData()
+    {
+        if (data == null) return;
+        if (!string.IsNullOrEmpty(PlayerPrefs.GetString(data.name)))
+            JsonUtility.FromJsonOverwrite(PlayerPrefs.GetString(data.name), data);
+    }
+}
