a7185c94cddd6611220b58a26971c2f82dc7c6d0
Lillian Wright <10803934@my.uvu.edu>
2020-03-01 18:10:24 -0700

    character controller practice

---
+++ Basics/Assets/Scripts/Character/CharacterMover.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMover : MonoBehaviour
+{
+    private CharacterController controller;
+    public Vector3 position;
+    public float speed = 10f;
+    public float jumpSpeed = 30f;
+    public int jumpCount = 0;
+    public float gravity = 3f;
+
+    public void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void Update()
+    {
+        if (controller.isGrounded)
+        {
+            position.y = 0;
+            jumpCount = 0;
+        }
+
+        position.x = Input.GetAxis("Horizontal") * speed;
+        if (Input.GetButtonDown("Space") && jumpCount)
+        {
+            position.y = jumpSpeed;
+            jumpCount++;
+        }
+
+        position.y -= gravity;
+        controller.Move(position * Time.deltaTime);
+    }
+}
