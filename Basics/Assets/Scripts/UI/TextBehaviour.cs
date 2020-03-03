64154fbabe6444dae41ea2a3fd32b02c9f6474e2
Lillian Wright <10803934@my.uvu.edu>
2020-03-03 12:44:28 -0700

    Dialogue

---
+++ Basics/Assets/Scripts/UI/TextBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TextBehaviour : MonoBehaviour
+{
+    
+    private Text textObj;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        textObj = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
