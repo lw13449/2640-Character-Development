a975abf14cf909a95842c8529e83a3b2d5c5e03d
Lillian Wright <10803934@my.uvu.edu>
2020-03-10 12:40:47 -0600

    collection

---
+++ Basics/Assets/Scripts/collections/Collection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Collection : ScriptableObject
+{
+    public List<Collectable> collectablesList;
+
+    public void AddToCollection(Collectable collectableObj)
+    {
+        //if (collectablesList.Contains(collectableObj))
+        //    return;
+        collectablesList.Add(collectableObj);
+    }
+
+    public void RemoveFromCollection(Collectable collectableObj)
+    {
+        for (var index =  collectablesList.Count - 1; index >= 0 ; index--)
+        {
+            var obj = collectablesList[index];
+            if (obj == collectableObj)
+            {
+                collectablesList.Remove(collectableObj);
+            }
+        }
+    }
+
+    public void ClearCollection ()
+    {
+        collectablesList.Clear();
+    }
+}
